2ab2a5b616f71cfcb01c10ff36c5585baf9d959b
mikaelgo <mikaelgo@hotmail.com>
2018-11-23 13:41:43 +0200

    fixed the Y coordinate of balls and the game now speeds up gets more difficult with every ball killed

--- main.js
+++ main.js
@@ -11,7 +11,8 @@ mainState.prototype = {
     create() {
         
         this.ballsMissed = 0;
-        this.score = 9001;
+        this.ballSpeed = -200;
+        this.score = 0;
         
         game.stage.backgroundColor = '#67BA26';
         /* Maybe these should not be here since they are not necessary this this state class
@@ -20,22 +21,21 @@ mainState.prototype = {
         */
         this.footballs = game.add.group();
         //bind function addOneFootball to the scope of this prototype (=same as class)
-        //so that we can ference the game properties inside the function
+        //so that we can reference the game properties inside the function
         this.addOneFootball = this.addOneFootball.bind(this)
         this.destroyFootball = this.destroyFootball.bind(this)
         
         this.timer = game.time.events.loop(1500,
-            this.addOneFootball
-        , this);    
+            this.addOneFootball, this);    
     },
     
     update() {
-        // console.log(this.score)
+       
     },
 
     
-    addOneFootball(x_old,y_old) {
-        var x = 400;
+    addOneFootball(x,y) {
+        var x = game.world.width;
         var y = 700;
         
         console.log("this " , this)
@@ -46,7 +46,7 @@ mainState.prototype = {
         this.footballs.add(football);
         
         game.physics.arcade.enable(football);
-        football.body.velocity.x = -200;
+        football.body.velocity.x = this.ballSpeed;
         football.checkWorldBounds = true;
         football.outOfBoundsKill = true;
         
@@ -55,9 +55,11 @@ mainState.prototype = {
     },
     
     destroyFootball(footballToBeDestroyed) {
+        //on every destroy player wil get one point
         this.score ++;
+        this.ballSpeed -= 10;
+        this.timer.delay -= 20;
         console.log("destroying this football " , footballToBeDestroyed, this.score)
-        //on every destroy playyer wil get one point
         footballToBeDestroyed.destroy();
         
     }
